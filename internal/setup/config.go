ecdd4263bd87412bb017d0aee9a338248691b738
Elizabeth Harper <foxcapade@gmail.com>
2021-05-03 10:58:07 -0400

    handle route creation

--- internal/setup/config.go
+++ internal/setup/config.go
@@ -1,9 +1,16 @@
 package setup
 
 import (
+	"fmt"
 	"os"
 
 	"gopkg.in/yaml.v3"
+)
+
+const (
+	errBadString = `Queue config entry %d missing required field "%s".`
+	errBadUint   = `Queue config entry %d field "%s" is missing or set to 0.` +
+		`  This value must be a positive, non-zero integer.`
 )
 
 type CLIConfig struct {
@@ -11,7 +18,7 @@ type CLIConfig struct {
 	QueueURL   string
 }
 
-func LoadQueueConfig(confPath string) map[string]Queue {
+func LoadQueueConfig(confPath string) map[string]QueueConfig {
 	file, err := os.Open(confPath)
 	bail(err)
 	defer file.Close()
@@ -20,12 +27,28 @@ func LoadQueueConfig(confPath string) map[string]Queue {
 	dec := yaml.NewDecoder(file)
 	bail(dec.Decode(&queues))
 
-	out := make(map[string]Queue, len(queues.Queues))
+	out := make(map[string]QueueConfig, len(queues.Queues))
 	for i := range queues.Queues {
-		if _, ok := out[queues.Queues[i].Name]; ok {
+		if len(queues.Queues[i].QueueName) == 0 {
+			panic(fmt.Sprintf(errBadString, i+1, "name"))
+		}
+
+		if queues.Queues[i].PollingInterval == 0 {
+			panic(fmt.Sprintf(errBadUint, i+1, "pollingInterval"))
+		}
+
+		if queues.Queues[i].MaxWorkers == 0 {
+			panic(fmt.Sprintf(errBadUint, i+1, "maxWorkers"))
+		}
+
+		if len(queues.Queues[i].Category) == 0 {
+			panic(fmt.Sprintf(errBadString, i+1, "name"))
+		}
+
+		if _, ok := out[queues.Queues[i].QueueName]; ok {
 			panic("Duplicate queue names in the queue config file.")
 		}
-		out[queues.Queues[i].Name] = queues.Queues[i]
+		out[queues.Queues[i].QueueName] = queues.Queues[i]
 	}
 
 	return out
--- internal/setup/queue.go
+++ internal/setup/queue.go
@@ -7,16 +7,23 @@ import (
 )
 
 type QueueWrapper struct {
-	Queues []Queue `yaml:"queues"`
+	Queues []QueueConfig `yaml:"queues"`
 }
 
-type Queue struct {
-	Name            string `json:"name" yaml:"name"`
+type QueueConfig struct {
+	QueueName       string `json:"name" yaml:"name"`
 	PollingInterval uint   `json:"polling_interval" yaml:"pollingInterval"`
 	MaxWorkers      uint   `json:"max_workers" yaml:"maxWorkers"`
+	Category        string `json:"job_category" yaml:"category"`
 }
 
-func LoadLiveQueues(url string) map[string]Queue {
+type QueueGet struct {
+	QueueName       string `json:"name"`
+	PollingInterval uint   `json:"polling_interval"`
+	MaxWorkers      uint   `json:"max_workers"`
+}
+
+func LoadLiveQueues(url string) map[string]QueueGet {
 	res := simple.GetRequest(prefixUrl(url) + "/queues").Submit()
 	bail(res.GetError())
 
@@ -24,20 +31,45 @@ func LoadLiveQueues(url string) map[string]Queue {
 		panic("unexpected response from queue server")
 	}
 
-	tmp := make([]Queue, 0, 5)
+	tmp := make([]QueueGet, 0, 5)
 	bail(res.UnmarshalBody(&tmp, simple.UnmarshallerFunc(json.Unmarshal)))
 
-	out := make(map[string]Queue, len(tmp))
+	out := make(map[string]QueueGet, len(tmp))
 	for i := range tmp {
-		out[tmp[i].Name] = tmp[i]
+		out[tmp[i].QueueName] = tmp[i]
 	}
 
 	return out
 }
 
-func SubmitQueue(url string, q Queue) {
-	res := simple.PutRequest(prefixUrl(url) + "/queue/" + q.Name).
-		MarshalBody(q, simple.MarshallerFunc(json.Marshal)).
+type QueuePut struct {
+	PollingInterval uint `json:"polling_interval"`
+	MaxWorkers      uint `json:"max_workers"`
+}
+
+func SubmitQueue(url string, q QueueConfig) {
+	res := simple.PutRequest(prefixUrl(url)+"/queue/"+q.QueueName).
+		MarshalBody(QueuePut{
+			PollingInterval: q.PollingInterval,
+			MaxWorkers:      q.MaxWorkers,
+		}, simple.MarshallerFunc(json.Marshal)).
+		Submit()
+	bail(res.GetError())
+
+	if res.MustGetResponseCode() != 200 {
+		panic("unexpected response from queue server: " + string(res.MustGetBody()))
+	}
+}
+
+type CategoryPut struct {
+	QueueName string `json:"name"`
+}
+
+func SubmitCategory(url string, q QueueConfig) {
+	res := simple.PutRequest(prefixUrl(url)+"/routing/"+q.Category).
+		MarshalBody(CategoryPut{
+			QueueName: q.QueueName,
+		}, simple.MarshallerFunc(json.Marshal)).
 		Submit()
 	bail(res.GetError())
 
